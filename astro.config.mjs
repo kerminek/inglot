eeade2cbcff678f723620880b7fb14c8f3cbbae3
kerminek <kermin0ps@gmail.com>
2024-01-30 20:00:25 +0100

    changes in astro config and fixing colors component bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,13 +7,14 @@ import node from "@astrojs/node";
 import solidJs from "@astrojs/solid-js";
 
 const IS_DEV = process.env.NODE_ENV === "development";
+console.log({ IS_DEV });
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), solidJs()],
   output: "hybrid",
   adapter: node({
-    mode: "middleware",
+    mode: "standalone",
   }),
   image: {
     domains: ["inglot.pl"],
