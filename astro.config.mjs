863b1ae080a91810793a5d3863da28f376cb6f83
kerminek <kermin0ps@gmail.com>
2024-01-30 20:31:01 +0100

    added vercel astro adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,23 +2,22 @@ import { defineConfig } from "astro/config";
 //
 import tailwind from "@astrojs/tailwind";
 //
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 import solidJs from "@astrojs/solid-js";
 
-const IS_DEV = process.env.NODE_ENV === "development";
-console.log({ IS_DEV });
+// const IS_DEV = process.env.NODE_ENV === "development";
+// console.log({ IS_DEV });
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), solidJs()],
   output: "hybrid",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
   image: {
     domains: ["inglot.pl"],
   },
-  site: IS_DEV ? undefined : "https://www.jakubkrwawicz.pl",
+  // site: IS_DEV ? undefined : "https://www.jakubkrwawicz.pl",
   base: IS_DEV ? undefined : "/portfolio-apps/inglot",
 });
